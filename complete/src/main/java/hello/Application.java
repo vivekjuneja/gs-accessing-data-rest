b401acd3d51c990c12c2a3ec22fef6eb6cbf48b5
vivekjuneja <vivekjuneja@gmail.com>
2015-06-18 08:10:26 +0900

    First commit

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,12 +1,19 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	
+    @Autowired
+    PersonRepository repository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	
 }
--- complete/src/main/java/hello/Person.java
+++ complete/src/main/java/hello/Person.java
@@ -14,7 +14,7 @@ public class Person {
 
 	private String firstName;
 	private String lastName;
-
+	
 	public String getFirstName() {
 		return firstName;
 	}
